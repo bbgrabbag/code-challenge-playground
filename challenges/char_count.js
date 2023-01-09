581a76ada586745d303fa1df4b7a4be8eaa7d68e
Ben Turner <bbgrabbag@gmail.com>
2023-01-09 16:30:19 -0700

    added interview q

---
+++ challenges/char_count.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {String} str
+ * @param {String} char
+ * @description - calculate the number of times 'char' appears in 'str'
+ * @returns {Number}
+ * 
+ * @example - charCount('abc', 'z') // --> 0
+ * @example - charCount('aaaa', 'a') // --> 4
+ */
+
+const charCount = (str, char) => {
+    
+}
+
+module.exports = charCount;
--- test-cases.js
+++ test-cases.js
@@ -1,14 +1,8 @@
 module.exports.testCases = {
     // Each test case property name corresponds to a file in ./challenges folder
-    example: [
-        // Each index is a tuple representing an individual test case
-        [
-            // Index 0 is array of function input values
-            [true],
-            // Index 1 is expected function output value
-            true
-        ],
-        [[false], false],
-        [['hello'], 'hello']
+    char_count: [
+        [['aaa', 'a'], 1],
+        [['abc', 'c'], 0],
+        [['abb', 'b'], 2],
     ]
 }
